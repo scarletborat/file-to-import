3c2f819d0ab758ac4c7bb5c1062d85dafa7e30b1
Scarlet <scarletborat@ukr.net>
2024-07-12 22:32:55 -0700

    make the script recursive

--- index.js
+++ index.js
@@ -11,34 +11,47 @@ const getOnlyFiles = (filesList) => {
   return filesList.filter(fileName => regex.test(fileName))
 }
 
-const createVarName = (name, separator = '-') => {
-  const capitalize = str => str.substring(0, 1).toUpperCase() + str.substring(1)
-  const tokens = name.split(separator)
-  const nameTail = tokens.slice(1).map(token => capitalize(token)).join('')
-  const varName = `${tokens[0]}${nameTail}`
-  return varName
-}
+(async function() {
+  const createVarName = (name, separator = '-') => {
+    const capitalize = str => str.substring(0, 1).toUpperCase() + str.substring(1)
+    const tokens = name.split(separator)
+    const nameTail = tokens.slice(1).map(token => capitalize(token)).join('')
+    const varName = `${tokens[0]}${nameTail}`
+    return varName
+  }
 
-const generateImport = (varName, path, fileName) => `import ${varName} from ~/${path}${fileName};`
-
-const createImports = (files, filePath) => {
-  const result = []
-  for (const file of files) {
+  const createImport = (file, basePath) => {
     const { name } = path.parse(file)
     const varName = createVarName(name)
-    const importStr = generateImport(varName, filePath, file)
-    result.push(importStr)
+    const relativePath = basePath ? file.replace(basePath, '') : file
+    const normalizedPath = path.normalize(relativePath)
+    const importStr = `import ${varName} from ~${normalizedPath};`
+    return importStr
   }
-  return result
-}
 
-(async function() {
+  const walkDirectory = async (dir, callback) => {
+    const files = await fs.readdir(dir, { withFileTypes: true });
+    
+    for (const file of files) {
+        const fullPath = path.join(dir, file.name);
+        if (file.isDirectory()) {
+            await walkDirectory(fullPath, callback);
+        } else {
+            await callback(fullPath);
+        }
+    }
+  }
+  
+  const createActionHandler = (basePath, result = []) => ({
+    getResult: () => result,
+    create: (file) => {
+      result.push(createImport(file, basePath))
+    }
+  })
+
   const [dir, basePath] = process.argv.slice(2)
-  const filePath = basePath
-    ? dir.replace(basePath, '')
-    : dir
-  let files = await listFiles(dir)
-  files = getOnlyFiles(files)
-  const result = createImports(files, filePath)
-  console.log(result.join('\n'))
+  const filePath = basePath ? dir.replace(basePath, '') : dir
+  const action = createActionHandler(filePath)
+  await walkDirectory(dir, action.create)
+  console.log(action.getResult())
 })()
